f5c21f9c82b9650b92fad66843483bb8536195dd
mor234 <moriya264@gmail.com>
2021-05-26 18:19:04 +0300

    got 100, still need impruving

--- Test.cpp
+++ Test.cpp
@@ -7,37 +7,27 @@
 using namespace ariel;
 using namespace std;
 
-
- 
-
-
-
-  
-int genrate_random_positive_negative() {
-
-}
-
-int genrate_random_double() {
-    double tmp_rand = ((double) rand() / (rand() + 0.1));
-    return tmp_rand * genrate_random_positive_negative();
-}
-
-
-TEST_CASE("good tests")
-{
+// TEST_CASE("good tests")
+// {
     
 
 
-}
+// }
 
 TEST_CASE("bad tests")
 {
     BinaryTree<int> tree_of_ints;
-    CHECK_NOTHROW(tree_of_ints.add_root(1));    
-    CHECK_NOTHROW(tree_of_ints.add_left(1, 9) );     // Now 9 is the left child of 1
+    CHECK_NOTHROW(tree_of_ints.add_root(1)); 
+    CHECK_THROWS(tree_of_ints.add_left(9,1) );     // Now 9 is the left child of 1
+    CHECK_NOTHROW(tree_of_ints.add_left(1, 9) );     //
     CHECK_NOTHROW(tree_of_ints.add_left(9, 4).add_right(9, 5) );     // Now 4 is the left child of 9
+    CHECK_THROWS(tree_of_ints.add_left(3,7) );     // 
     CHECK_NOTHROW(tree_of_ints.add_right(9, 5)  );   // Now 5 is the right child of 9
+    CHECK_THROWS(tree_of_ints.add_right(3, 5)  );   //
     CHECK_NOTHROW(tree_of_ints.add_right(1, 3) );    // Now 3 is the right child of 1
+    CHECK_THROWS(tree_of_ints.add_right(20, 19)  );   //
+    CHECK_THROWS(tree_of_ints.add_right(30, 5)  );   //
+
     CHECK_NOTHROW(tree_of_ints.add_left(1, 2));     // Now 2 is the left child of 1, instead of 9 (the children of 9 remain in place)
     CHECK_NOTHROW(cout << tree_of_ints << endl);  /* Prints the tree in a reasonable format. For example:*/
     vector<int> vec_check{};
@@ -54,75 +44,95 @@ TEST_CASE("bad tests")
     CHECK_EQ(vec_check,vec_ans);
     vec_check.clear();
     vec_ans.clear();
-     vec_ans.push_back(4);
+    vec_ans.push_back(4);
     vec_ans.push_back(2);
     vec_ans.push_back(5);
     vec_ans.push_back(1);
     vec_ans.push_back(3);
     for (auto it=tree_of_ints.begin_inorder(); it!=tree_of_ints.end_inorder(); ++it) {
         vec_check.push_back((*it));
-  }  // prints: 4 2 5 1 3 
-      CHECK_EQ(vec_check,vec_ans);
+    }  // prints: 4 2 5 1 3 
+    CHECK_EQ(vec_check,vec_ans);
     vec_check.clear();
 
-  for (int element: tree_of_ints) {  // this should work like inorder
-        vec_check.push_back((element));
-  }  // prints: 4 2 5 1 3 
-      CHECK_EQ(vec_check,vec_ans);
+    for (int element: tree_of_ints) {  // this should work like inorder
+            vec_check.push_back((element));
+    }  // prints: 4 2 5 1 3 
+    CHECK_EQ(vec_check,vec_ans);
 
 
- vec_check.clear();
+    vec_check.clear();
     vec_ans.clear();
-     vec_ans.push_back(4);
+    vec_ans.push_back(4);
     vec_ans.push_back(5);
     vec_ans.push_back(2);
     vec_ans.push_back(3);
     vec_ans.push_back(1);
 
 
-for (auto it=tree_of_ints.begin_postorder(); it!=tree_of_ints.end_postorder(); ++it) {
-        vec_check.push_back((*it));
-  }  // prints: 4 5 2 3 1
-   
-      CHECK_EQ(vec_check,vec_ans);
+    for (auto it=tree_of_ints.begin_postorder(); it!=tree_of_ints.end_postorder(); ++it) {
+            vec_check.push_back((*it));
+    }  // prints: 4 5 2 3 1
 
-  // The same should work with other types, e.g. with strings:
-  vector<string> vec_check_str{};
+    CHECK_EQ(vec_check,vec_ans);
+
+    // The same should work with other types, e.g. with strings:
+    vector<string> vec_check_str{};
     vector<string> vec_ans_str{};
-  BinaryTree<string> tree_of_strings;
-  tree_of_strings.add_root("1")     
-  .add_left("1", "9")      // Now 9 is the left child of 1
-  .add_left("9", "4")      // Now 4 is the left child of 9
-  .add_right("9", "5")     // Now 5 is the right child of 9
-  .add_right("1", "3")     // Now 3 is the right child of 1
-  .add_left("1", "2");     // Now 2 is the left child of 1, instead of 9 (the children of 9 remain in place)
-  CHECK_NOTHROW( cout << tree_of_strings << endl); 
-   vec_ans_str.push_back("1");
+    BinaryTree<string> tree_of_strings;
+    tree_of_strings.add_root("1")     
+    .add_left("1", "9")      // Now 9 is the left child of 1
+    .add_left("9", "4")      // Now 4 is the left child of 9
+    .add_right("9", "5")     // Now 5 is the right child of 9
+    .add_right("1", "3")     // Now 3 is the right child of 1
+    .add_left("1", "2");     // Now 2 is the left child of 1, instead of 9 (the children of 9 remain in place)
+    CHECK_NOTHROW( cout << tree_of_strings << endl); 
+    vec_ans_str.push_back("1");
     vec_ans_str.push_back("2");
     vec_ans_str.push_back("4");
     vec_ans_str.push_back("5");
     vec_ans_str.push_back("3");
 
-  for (auto it=tree_of_strings.begin_preorder(); it!=tree_of_strings.end_preorder(); ++it) {
-   vec_check_str.push_back(*it);
-  }  // prints: 1 2 4 5 3
-        CHECK_EQ(vec_check_str,vec_ans_str);
+    for (auto it=tree_of_strings.begin_preorder(); it!=tree_of_strings.end_preorder(); ++it) {
+    vec_check_str.push_back(*it);
+    }  // prints: 1 2 4 5 3
+    CHECK_EQ(vec_check_str,vec_ans_str);
+    vec_check_str.clear();
+    vec_ans_str.clear();
+    vec_ans_str.push_back("4");
+    vec_ans_str.push_back("2");
+    vec_ans_str.push_back("5");
+    vec_ans_str.push_back("1");
+    vec_ans_str.push_back("3");
 
-  for (auto it=tree_of_strings.begin_inorder(); it!=tree_of_strings.end_inorder(); ++it) {
-    cout << (*it) << " " ;
-  }  // prints: 4 2 5 1 3 
-  for (auto it=tree_of_strings.begin_postorder(); it!=tree_of_strings.end_postorder(); ++it) {
-    cout << (*it) << " " ;
-  }  // prints: 4 5 2 3 1
+    for (auto it=tree_of_strings.begin_inorder(); it!=tree_of_strings.end_inorder(); ++it) {
+        vec_check_str.push_back(*it);
+    }  // prints: 4 2 5 1 3 
+    CHECK_EQ(vec_check_str,vec_ans_str);
+    vec_check_str.clear();
+    for (const string& element: tree_of_strings) {  // this should work like inorder
+        vec_check_str.push_back(element);
+    }   // prints: 4 2 5 1 3 
+    CHECK_EQ(vec_check_str,vec_ans_str);
+    vec_check_str.clear();
+    vec_ans_str.clear();
+    vec_ans_str.push_back("4");
+    vec_ans_str.push_back("5");
+    vec_ans_str.push_back("2");
+    vec_ans_str.push_back("3");
+    vec_ans_str.push_back("1");
+    for (auto it=tree_of_strings.begin_postorder(); it!=tree_of_strings.end_postorder(); ++it) {
+        vec_check_str.push_back(*it);
+    }  // prints: 4 5 2 3 1
 
-  // demonstrate the arrow operator:
-  for (auto it=tree_of_strings.begin_postorder(); it!=tree_of_strings.end_postorder(); ++it) {
-    cout << it->size() << " " ;
-  }  // prints: 1 1 1 1 1 
+    // demonstrate the arrow operator:
+    for (auto it=tree_of_strings.begin_postorder(); it!=tree_of_strings.end_postorder(); ++it) {
+       CHECK_EQ(it->size(),1);
+    }  // prints: 1 1 1 1 1 
 
 
-  for (const string& element: tree_of_strings) {  // this should work like inorder
-    cout << element << " " ;
-  }   // prints: 4 2 5 1 3 
+    for (const string& element: tree_of_strings) {  // this should work like inorder
+        cout << element << " " ;
+    }   // prints: 4 2 5 1 3 
 
 }
